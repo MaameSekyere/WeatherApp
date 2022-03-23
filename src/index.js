52c01dc601e8533deb5f65269907623f314dfada
MaameSekyere <maameant@gmail.com>
2022-03-23 05:05:23 -0400

    added actual date and time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+function showDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay(3)];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemp(response) {
   console.log(response.data);
   let temperatureEl = document.querySelector("#temperature");
@@ -5,11 +28,13 @@ function displayTemp(response) {
   let descriptionEl = document.querySelector("#description");
   let humidityEl = document.querySelector("#humidity");
   let windEl = document.querySelector("#wind");
+  let dateEl = document.querySelector("#date");
   temperatureEl.innerHTML = Math.round(response.data.main.temp);
   cityEl.innerHTML = response.data.name;
   descriptionEl.innerHTML = response.data.weather[0].description;
   humidityEl.innerHTML = response.data.main.humidity;
   windEl.innerHTML = Math.round(response.data.wind.speed);
+  dateEl.innerHTML = showDate(response.data.dt * 1000);
 }
 
 let apiKey = "573a564668504c3a4328912c04e8e7e5";
