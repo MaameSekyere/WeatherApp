ec68a5eb7f0f3db629f14e164bf58071b349a9f9
MaameSekyere <maameant@gmail.com>
2022-03-23 02:44:09 -0400

    added api in js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
+let apiKey = "573a564668504c3a4328912c04e8e7e5";
+let city = "Buffalo";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
+console.log(apiUrl);
