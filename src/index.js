3439f521ea0e043748ea38b004b81b3f18fce358
MaameSekyere <maameant@gmail.com>
2022-04-15 04:16:15 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -30,7 +30,6 @@ function formatDay(timestamp) {
 }
 
 function displayWeatherForecast(response) {
-  console.log(response.data.daily);
   let forecast = response.data.daily;
   let forecastEl = document.querySelector("#weather-forecast");
 
@@ -71,7 +70,6 @@ function displayWeatherForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "573a564668504c3a4328912c04e8e7e5";
   let apiUrl = `
   https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial
